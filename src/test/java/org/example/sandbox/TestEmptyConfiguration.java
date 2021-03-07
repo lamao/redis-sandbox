f2453774ae874c1b5fda403c6f875bb661b39cdd
vyacheslav.mischeryakov <vyacheslav.mischeryakov@pragmaticplay.com>
2021-03-07 10:38:21 +0200

    Initial commit

---
+++ src/test/java/org/example/sandbox/TestEmptyConfiguration.java
@@ -0,0 +1,14 @@
+package org.example.sandbox;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestEmptyConfiguration {
+
+    @Test
+    public void test() {
+        assertTrue("Project setup is not successful", true);
+    }
+
+}
